da04a9f5b930af5c83f5e5d8877509bc755eaa59
Laine Elliott <lse246@nyu.edu>
2019-04-29 11:41:35 -0400

    added click and drag, and started zoom/new screen function

---
+++ Assets/scripts/ClickZoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickZoom : MonoBehaviour
+{
+
+    public Camera Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Camera.transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z + 20));
+    }
+}
