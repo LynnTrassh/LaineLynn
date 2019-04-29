8cd37b58c2ab9e25dffc2330293a4052693d8104
Laine Elliott <lse246@nyu.edu>
2019-04-29 10:47:18 -0400

    added camera control buttons for room navigation

---
+++ Assets/scripts/CameraManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CameraManager : MonoBehaviour
+{
+
+    public Button rightArrow;
+    public Button leftArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void RightArrowClick()
+    {
+        if(transform.position.x >= 12)
+        {
+            transform.position = new Vector3(-23, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x + 12, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void LeftArrowClick()
+    {
+        if (transform.position.x <= -23)
+        {
+            transform.position = new Vector3(13, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x - 12, transform.position.y, transform.position.z);
+        }
+    }
+}
