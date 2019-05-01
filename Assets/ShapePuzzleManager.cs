d1395e842187b007fd70c08223d2fc7f86a3dbeb
lynntrassh <lj1072@nyu.edu>
2019-05-01 14:48:56 -0400

    PuzzleGameWorking!

---
+++ Assets/ShapePuzzleManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapePuzzleManager : MonoBehaviour
+{
+    public static ShapePuzzleManager Instance = null;
+
+    public bool[] settled;
+    public GridNode[] gridNodes;
+    public Tile[] tiles;
+
+    public Tile lastDraggedTile;
+    public float thresholdDistance;
+
+    
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(settled[0] == true && settled[1] == true && settled[2] == true && settled[3] == true && settled[4] == true && settled[5] == true)
+        {
+            Debug.Log("Win!!");
+        }
+    }
+
+    public void CheckTileDistances()
+    {
+        for (int i = 0; i < gridNodes.Length; i++)
+        {
+            float distance = Vector3.Distance(gridNodes[i].transform.position, lastDraggedTile.transform.position);
+            Debug.Log("distance: " + distance);
+            if (distance < thresholdDistance)
+            {
+                
+                SnapToGridPoint(gridNodes[i]);
+                break;
+            }
+        }
+    }
+
+    void SnapToGridPoint (GridNode gridNode)
+    {
+        lastDraggedTile.transform.position = gridNode.transform.position;
+        //gridNode.tileOnNode = lastDraggedTile;
+        ////for (int i =  0; i < gridNodes.Length; i++)
+        /*if(gridNode.answerNumber == lastDraggedTile.number)
+        {
+            
+            Debug.Log("score = " + score);
+            Destroy(lastDraggedTile.GetComponent<ClickDrag>());
+            
+
+        }
+        */
+
+        if(gridNode.answerNumber == 1 && lastDraggedTile.number == 1)
+        {
+            settled[0] = true;
+        }
+        else if (gridNode.answerNumber == 2 && lastDraggedTile.number == 2)
+        {
+            settled[1] = true;
+        }
+        else if (gridNode.answerNumber == 3 && lastDraggedTile.number == 3)
+        {
+            settled[2] = true;
+        }
+        else if (gridNode.answerNumber == 4 && lastDraggedTile.number == 4)
+        {
+            settled[3] = true;
+        }
+        else if (gridNode.answerNumber == 5 && lastDraggedTile.number == 5)
+        {
+            settled[4] = true;
+        }
+        else if (gridNode.answerNumber == 6 && lastDraggedTile.number == 6)
+        {
+            settled[5] = true;
+        }
+
+
+    }
+}
--- Assets/scripts/ClickDrag.cs
+++ Assets/scripts/ClickDrag.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ClickDrag : MonoBehaviour
 {
-
+    
     private Vector3 click;
     // Start is called before the first frame update
     void Start()
